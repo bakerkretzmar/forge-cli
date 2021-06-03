6ecad813d961fac582c008bcb2845809067e642b
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-06-03 19:45:37 -0400

    Rework base `ForgeCommand`

--- app/Commands/ForgeCommand.php
+++ app/Commands/ForgeCommand.php
@@ -2,12 +2,47 @@
 
 namespace App\Commands;
 
-use App\Commands\Concerns\EnsureHasForgeConfiguration;
-use App\Commands\Concerns\NeedsForgeToken;
+use App\Support\Configuration;
 use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class ForgeCommand extends Command
 {
-    use NeedsForgeToken;
-    use EnsureHasForgeConfiguration;
+    protected string $environment;
+
+    protected Configuration $config;
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (blank(config('forge.token'))) {
+            $this->error('No Forge API token found. Run `forge login` first.');
+
+            return static::FAILURE;
+        }
+
+        if (! file_exists(getcwd() . '/forge.yml')) {
+            $this->error('No `forge.yml` configuration file found. Run `forge init` first.');
+
+            return static::FAILURE;
+        }
+
+        $this->environment = $this->argument('environment');
+        $this->config = app(Configuration::class)->setEnvironment($this->environment);
+
+        return parent::execute($input, $output);
+    }
+
+    protected function getArguments()
+    {
+        return [
+            new InputArgument('environment', InputArgument::OPTIONAL, 'The environment to run the command in.', 'production'),
+        ];
+    }
+
+    protected function emphasize(string $text): string
+    {
+        return "<options=bold;fg=blue>{$text}</options=bold;fg=blue>";
+    }
 }
